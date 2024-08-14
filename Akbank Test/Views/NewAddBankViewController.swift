66d403521da44eedef6766cd22bbbdfeeb36e3e2
Görkem Karagöz <gorkem1928karagoz@gmail.com>
2024-08-14 11:29:17 +0300

    Firebase Storage and Firestore Database have been added to Akbank Test application. The user will be able to see the list of banks added with the data stored in Firebase.

--- Akbank Test/Views/NewAddBankViewController.swift
+++ Akbank Test/Views/NewAddBankViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Görkem Karagöz on 2.08.2024.
 //
-
+/*
 import UIKit
 
 class NewAddBankViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
@@ -77,6 +77,132 @@ class NewAddBankViewController: UIViewController, UITableViewDelegate, UITableVi
         alert.addAction(cancel)
         
         present(alert, animated: true, completion: nil)
+    }
+}
+*/
+import UIKit
+import Firebase
+import FirebaseStorage
+import FirebaseAuth
+import FirebaseCore
+
+class NewAddBankViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    struct NewBank {
+        let title: String
+        let imageName: String
+    }
+    
+    var data: [NewBank] = []
+    var addedBanks: Set<String> = [] // Eklenen bankaları takip etmek için
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        fetchAddedBanks() // Kullanıcının eklediği bankaları çek
+    }
+    
+    func fetchAddedBanks() {
+        guard let userId = Auth.auth().currentUser?.uid else { return }
+        
+        let db = Firestore.firestore()
+        db.collection("users").document(userId).collection("addedBanks").getDocuments { (querySnapshot, error) in
+            if let error = error {
+                print("Error getting added banks: \(error)")
+            } else {
+                self.addedBanks = Set(querySnapshot?.documents.compactMap { document -> String? in
+                    let data = document.data()
+                    return data["name"] as? String
+                } ?? [])
+                
+                self.fetchBanks() // Bankaları eklenenlere göre güncelle
+            }
+        }
+    }
+    
+    func fetchBanks() {
+        let db = Firestore.firestore()
+        
+        db.collection("Banks").getDocuments { (querySnapshot, error) in
+            if let error = error {
+                print("Error getting documents: \(error)")
+            } else {
+                self.data = querySnapshot?.documents.compactMap { document -> NewBank? in
+                    let data = document.data()
+                    guard let title = data["name"] as? String, let imageName = data["logoUrl"] as? String else {
+                        return nil
+                    }
+                    // Eklenen bankaları hariç tut
+                    guard !self.addedBanks.contains(title) else { return nil }
+                    return NewBank(title: title, imageName: imageName)
+                } ?? []
+                
+                // Reload the table view on the main thread
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let newBank = data[indexPath.row]
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CustomTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.label.text = newBank.title
+        
+        let storageRef = Storage.storage().reference().child("bank_icons/\(newBank.imageName)")
+        
+        storageRef.getData(maxSize: 1 * 1024 * 1024) { data, error in
+            if let error = error {
+                print("Error downloading image: \(error)")
+            } else if let data = data, let image = UIImage(data: data) {
+                cell.iconİmageView.image = image
+            }
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedBank = data[indexPath.row].title
+        guard let userId = Auth.auth().currentUser?.uid else { return }
+        
+        let db = Firestore.firestore()
+        db.collection("users").document(userId).collection("addedBanks").addDocument(data: [
+            "name": selectedBank
+        ]) { error in
+            if let error = error {
+                print("Error adding bank: \(error)")
+            } else {
+                print("\(selectedBank) bankası başarıyla eklendi.")
+                
+                // Veriyi başarıyla ekledikten sonra, önceki ekrana dönüp tabloyu güncelle
+                if let newBanksVC = self.navigationController?.viewControllers.first(where: { $0 is NewBanksViewController }) as? NewBanksViewController {
+                    newBanksVC.fetchUserBanks()
+                }
+                
+                // Uyarı ve geri dönüş
+                let alert = UIAlertController(title: "Banka Eklendi", message: "\(selectedBank) bankası başarıyla eklendi.", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "Tamam", style: .default) { _ in
+                    self.navigationController?.popViewController(animated: true)
+                }
+                alert.addAction(okAction)
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
--- Akbank Test/Views/NewBanksViewController.swift
+++ Akbank Test/Views/NewBanksViewController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Görkem Karagöz on 1.08.2024.
 //
+/*
 import UIKit
 
 class NewBanksViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
@@ -38,3 +39,151 @@ class NewBanksViewController: UIViewController,UITableViewDelegate,UITableViewDa
     }
     
 }
+*/
+import UIKit
+import Firebase
+import DZNEmptyDataSet
+import FirebaseAuth
+import FirebaseFirestore
+
+class NewBanksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, DZNEmptyDataSetSource, DZNEmptyDataSetDelegate {
+
+    var banks: [String] = []
+    var shouldShowEmptyDataSet = false
+    var isLoading = true
+
+    @IBOutlet weak var tableview: UITableView!
+    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableview.dataSource = self
+        tableview.delegate = self
+        tableview.emptyDataSetSource = self
+        tableview.emptyDataSetDelegate = self
+        tableview.tableFooterView = UIView() // Boş hücreleri gizlemek için
+        
+        tableview.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        fetchUserBanks()
+    }
+    
+    @IBAction func addButton(_ sender: Any) {
+        performSegue(withIdentifier: "toNewAddBankVC", sender: nil)
+    }
+    
+    func fetchUserBanks() {
+        indicatorView.startAnimating()
+        indicatorView.isHidden = false
+        isLoading = true
+        
+        guard let userId = Auth.auth().currentUser?.uid else { return }
+        
+        let db = Firestore.firestore()
+        db.collection("users").document(userId).collection("addedBanks").getDocuments { (querySnapshot, error) in
+            if let error = error {
+                print("Error getting documents: \(error)")
+            } else {
+                self.banks = querySnapshot?.documents.compactMap { document -> String? in
+                    let data = document.data()
+                    return data["name"] as? String
+                } ?? []
+                
+                self.shouldShowEmptyDataSet = self.banks.isEmpty
+                self.isLoading = false
+                
+                DispatchQueue.main.async {
+                    self.updateTableView()
+                }
+            }
+        }
+    }
+
+    func updateTableView() {
+        tableview.reloadData()
+        indicatorView.stopAnimating()
+        indicatorView.isHidden = true
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return banks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = banks[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            guard let userId = Auth.auth().currentUser?.uid else { return }
+            let bankNameToDelete = banks[indexPath.row]
+
+            let db = Firestore.firestore()
+            db.collection("users").document(userId).collection("addedBanks").whereField("name", isEqualTo: bankNameToDelete).getDocuments { (querySnapshot, error) in
+                if let error = error {
+                    print("Error deleting document: \(error)")
+                } else if let document = querySnapshot?.documents.first {
+                    document.reference.delete { error in
+                        if let error = error {
+                            print("Error removing document: \(error)")
+                        } else {
+                            self.banks.remove(at: indexPath.row)
+                            DispatchQueue.main.async {
+                                self.updateTableView()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    // DZNEmptyDataSetSource metodları
+    func emptyDataSetShouldDisplay(_ scrollView: UIScrollView!) -> Bool {
+        return !isLoading && shouldShowEmptyDataSet
+    }
+
+    func title(forEmptyDataSet scrollView: UIScrollView) -> NSAttributedString? {
+        let title = "Herhangi bir banka eklenmedi."
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 18, weight: .medium),
+            .foregroundColor: UIColor.gray
+        ]
+        return NSAttributedString(string: title, attributes: attributes)
+    }
+    
+    func description(forEmptyDataSet scrollView: UIScrollView) -> NSAttributedString? {
+        let description = "Lütfen yeni bir banka ekleyin."
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 14),
+            .foregroundColor: UIColor.lightGray
+        ]
+        return NSAttributedString(string: description, attributes: attributes)
+    }
+    
+    func image(forEmptyDataSet scrollView: UIScrollView) -> UIImage? {
+        return UIImage(systemName: "banknote") // Placeholder için ikon ekleyebilirsiniz
+    }
+    
+    func emptyDataSetShouldAllowTouch(_ scrollView: UIScrollView) -> Bool {
+        return true
+    }
+    
+    func emptyDataSetShouldAllowScroll(_ scrollView: UIScrollView) -> Bool {
+        return true
+    }
+}
+
+
