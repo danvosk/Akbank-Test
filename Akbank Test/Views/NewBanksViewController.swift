568dfba915274fd03243e9e4a6f0605f43f36039
Görkem Karagöz <gorkem1928karagoz@gmail.com>
2024-08-14 11:31:01 +0300

    Unnecessary comment lines deleted

--- Akbank Test/Views/NewBanksViewController.swift
+++ Akbank Test/Views/NewBanksViewController.swift
@@ -4,42 +4,6 @@
 //
 //  Created by Görkem Karagöz on 1.08.2024.
 //
-/*
-import UIKit
-
-class NewBanksViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-
-    
-    let banks = ["Garanti BBVA","QNB Finansbank","İş Bankası","VakıfBank"]
-    
-    @IBOutlet weak var tableview: UITableView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-       tableview.dataSource = self
-       tableview.delegate = self
-    
-        tableview.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-    }
-
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return banks.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = banks[indexPath.row]
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-    
-}
-*/
 import UIKit
 import Firebase
 import DZNEmptyDataSet
